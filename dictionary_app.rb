066ec3dd8f5236260e07facdca0a73924a619249
Donnell Reuben <117698398+donnellreuben@users.noreply.github.com>
2023-03-24 15:10:53 -0500

    Update dictionary_app.rb

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -5,7 +5,7 @@ response = HTTP.get('https://api.wordnik.com/v4/word.json/corn/definitions?limit
 
 puts "Please enter a word: "
 user_input = gets.chomp
-custom_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=22bf12vddbpizrmyfs90cf695e9j4u1seq52o4klisqmqn6v2")
+custom_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 
 index = 0
 defintion = custom_response.parse(:json)[index]["text"]
